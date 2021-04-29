daf0f68c28e90be64a5d92dba03b2a956672681f
Steven Wang <skykk0128@gmail.com>
2021-04-29 19:19:48 +1000

    PWV-0002: Announcement Bar and Header Design

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Popup
+// Show / Hide Popup
 const handlePopup = (element) => {
 	const popupWindow = document.getElementsByClassName(element)[0];
 	popupWindow.classList.add("show");
@@ -9,4 +9,13 @@ const handleClose = (element) => {
 	const popupWindow = document.getElementsByClassName(element)[0];
 	popupWindow.classList.add("hidden");
 	popupWindow.classList.remove("show");
+}
+
+// Send Subscription to Email
+const subscribeEmail = () => {
+    const link = "mailto:" + encodeURIComponent(document.getElementById('email').value)
+             + "?cc=skykk0128@gmail.com"
+             + "&subject=" + encodeURIComponent("Subscribe Newletters from Steven Wang")
+             + "&body=" + encodeURIComponent("Subscribe Newletters from Steven Wang");
+    window.location.href = link;
 }
