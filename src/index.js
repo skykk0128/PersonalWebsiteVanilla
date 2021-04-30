af42582feb4aa078a0bfc7c432d605efe5e82e79
Steven Wang <skykk0128@gmail.com>
2021-04-30 19:25:12 +1000

    PWV-0001: Initialise Project

--- src/index.js
+++ src/index.js
@@ -19,3 +19,17 @@ const subscribeEmail = () => {
              + "&body=" + encodeURIComponent("Subscribe Newletters from Steven Wang");
     window.location.href = link;
 }
+
+// Load Random Avatar
+const getRandomAvatar = () => {
+	var avatars = ["aquaman.svg", "atom.svg", "batman-dark.svg", "batman.svg", "catwoman-dark.svg", "catwoman.svg",
+	"cyborg-dark.svg", "cyborg.svg", "green-arrow-dc.svg", "green-lantern-dc-dark.svg", "green-lantern-dc.svg", 
+	"harley-quinn-dc.svg", "hawkgirl-dark.svg", "hawkgirl.svg", "hawkman.svg", "isis.svg", "joker-dc-dark.svg", 
+	"joker-dc.svg", "lex-luthor-dark.svg", "lex-luthor.svg", "nightwing.svg", "penguin-dc.svg", "superman-dc-dark.svg",
+	"superman-dc.svg", "the-flash-dark.svg", "the-flash.svg", "zatanna.svg"];
+	return avatars[Math.floor(Math.random() * avatars.length)];
+}
+
+const randomAvatar = () => {
+	document.getElementById("avatar").src = "/src/assets/svgs/icon_avatar/" + getRandomAvatar();
+}
