cae7f072c7193b5f20862417005847b89f6d6e4a
Steven Wang <skykk0128@gmail.com>
2021-05-01 13:28:11 +1000

    PWV-0002: Announcement Bar, Popup and Header Design

--- src/index.js
+++ src/index.js
@@ -9,15 +9,76 @@ const handleClose = (element) => {
 	const popupWindow = document.getElementsByClassName(element)[0];
 	popupWindow.classList.add("hidden");
 	popupWindow.classList.remove("show");
+	document.getElementById("email__error").style.visibility = "hidden";
+	document.getElementById("login__email__error").style.visibility = "hidden";
+	document.getElementById("login__password__error").style.visibility = "hidden";
 }
 
 // Send Subscription to Email
 const subscribeEmail = () => {
-    const link = "mailto:" + encodeURIComponent(document.getElementById('email').value)
+	const emailValue = document.subscription.email.value;
+	if (emailValue === "") {
+		document.getElementById("email__error").style.visibility = "visible";
+		return false;
+	} else {
+		document.getElementById("email__error").style.visibility = "hidden";
+		const link = "mailto:" + encodeURIComponent(document.getElementById('email').value)
              + "?cc=skykk0128@gmail.com"
              + "&subject=" + encodeURIComponent("Subscribe Newletters from Steven Wang")
              + "&body=" + encodeURIComponent("Subscribe Newletters from Steven Wang");
-    window.location.href = link;
+    	window.location.href = link;
+		return true;
+	}
+}
+
+// Login Empty Input Handler
+const handleLoginEmpty = () => {
+	const loginEmailValue = document.login.loginemail.value;
+	const loginPasswordValue = document.login.loginpassword.value;
+	if (loginEmailValue === "") {
+		document.getElementById("login__email__error").style.visibility = "visible";
+	} else {
+		document.getElementById("login__email__error").style.visibility = "hidden";
+	}
+	if (loginPasswordValue === "") {
+		document.getElementById("login__password__error").style.visibility = "visible";
+	} else {
+		document.getElementById("login__password__error").style.visibility = "hidden";
+	}
+}
+
+// Register Empty Input Handler
+const handleRegisterEmpty = () => {
+	const registerNameValue = document.register.registername.value;
+	const registerUsernameValue = document.register.registerusername.value;
+	const registerEmailValue = document.register.registeremail.value;
+	const registerPasswordValue = document.register.registerpassword.value;
+	const confirmPasswordValue = document.register.confirmpassword.value;
+	if (registerNameValue === "") {
+		document.getElementById("register__name__error").style.visibility = "visible";
+	} else {
+		document.getElementById("register__name__error").style.visibility = "hidden";
+	}
+	if (registerUsernameValue === "") {
+		document.getElementById("register__username__error").style.visibility = "visible";
+	} else {
+		document.getElementById("register__username__error").style.visibility = "hidden";
+	}
+	if (registerEmailValue === "") {
+		document.getElementById("register__email__error").style.visibility = "visible";
+	} else {
+		document.getElementById("register__email__error").style.visibility = "hidden";
+	}
+	if (registerPasswordValue === "") {
+		document.getElementById("register__password__error").style.visibility = "visible";
+	} else {
+		document.getElementById("register__password__error").style.visibility = "hidden";
+	}
+	if (confirmPasswordValue === "") {
+		document.getElementById("confirm__password__error").style.visibility = "visible";
+	} else {
+		document.getElementById("confirm__password__error").style.visibility = "hidden";
+	}
 }
 
 // Load Random Avatar
